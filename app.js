f3c0d42148be4c506bd62b26113aff657121c064
Enoch <lim_enoch@students.edu.sg>
2023-09-06 21:29:58 +0800

    render pages using express js

--- app.js
+++ app.js
@@ -1,36 +1,32 @@
-const http = require("http");
 const fs = require("fs");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  switch (req.url) {
-    case "/":
-      res.setHeader("Content-Type", "text/html");
-      fs.readFile("./public/index.html", (err, data) => {
-        if (err) {
-          console.log(err);
-          res.end();
-        } else {
-          res.setHeader("Content-Type", "text/html");
-          res.write(data);
-          res.end();
-        }
-      });
-      break;
-    case "/about":
-      res.write("About us");
-      res.end();
-      break;
-    case "/contact":
-      res.write("Contact us");
-      res.end();
-      break;
-    default:
-      res.write("404 not found");
-      res.end();
-  }
+const app = express();
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-const PORT = 3000;
-server.listen(PORT, "localhost", () => {
-  console.log(`Server is listening on http://localhost:${PORT}`);
+app.get("/", (req, res) => {
+  res.redirect("home");
+});
+
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/chat", (req, res) => {
+  res.render("chat");
+});
+
+app.use((req, res) => {
+  res.render("404");
 });
