5a7a0382f1438427f01b80c83003807efaa07201
Enoch <lim_enoch@students.edu.sg>
2023-09-06 19:40:42 +0800

    Server code

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-const Http = require("http");
+const http = require("http");
 
-const server = Http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  console.log("request made");
+  res.write("Never gonna give u up");
+  res.end();
+});
+
+const PORT = 3000;
+server.listen(PORT, "localhost", () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
