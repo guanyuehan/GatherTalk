e407504b07affc5a90f8ed385cfe300530aae7e8
Enoch <lim_enoch@students.edu.sg>
2023-09-06 16:25:24 +0800

    comment input

--- src/main.js
+++ src/main.js
@@ -14,10 +14,3 @@ if (saved != null) {
 }
 
 chat.appendChild(mainThreadElement);
-
-function addComment() {
-  let date = new Date();
-  const comment = new Comment(1, "This is a comment", "Tricia", date, null);
-  mainThread.addComment(comment);
-  refreshPage();
-}
