4981e9d5ca291bebd7fef88f47fe2b234259aafd
Enoch <lim_enoch@students.edu.sg>
2023-09-06 20:03:36 +0800

    Changed localstorage to new tab(RR)

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log("request made");
-  res.write("Never gonna give u up");
-  res.end();
+  switch (req.url) {
+    case "/":
+      res.setHeader("Content-Type", "text/html");
+      fs.readFile("./public/index.html", (err, data) => {
+        if (err) {
+          console.log(err);
+          res.end();
+        } else {
+          res.setHeader("Content-Type", "text/html");
+          res.write(data);
+          res.end();
+        }
+      });
+      break;
+    case "/about":
+      res.write("About us");
+      res.end();
+      break;
+    case "/contact":
+      res.write("Contact us");
+      res.end();
+      break;
+    default:
+      res.write("404 not found");
+      res.end();
+  }
 });
 
 const PORT = 3000;
